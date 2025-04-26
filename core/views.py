53cf084ddf96068dd59c430c6ce01a0a56b878ba
daniel <dani.roman@duocuc.cl>
2025-04-25 20:56:05 -0400

    cambio de vistas agregadas

--- core/views.py
+++ core/views.py
@@ -6,3 +6,19 @@ from django.shortcuts import render
 
 def inicio(request):
         return render(request, 'core/inicio.html')
+
+def registro(request):
+        return render (request, 'core/registro.html')
+
+def login(request):
+        return render (request, 'core/login.html')
+
+def hospedaje(request):
+        return render (request, 'core/hospedaje.html')
+
+def actividad(request):
+        return render (request, 'core/actividad.html')
+
+def gastronomia(request):
+        return render (request, 'core/gastronomia.html')
+
