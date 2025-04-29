f50887e79b9887356161e194654e5a50f6e854b1
barbara <bar.carrasco@duocuc.cl>
2025-04-29 13:34:01 -0400

    hospedaje

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,11 @@
 from django.urls import path
 from .views import *
 
-
 urlpatterns = [
     path('', inicio, name='inicio'),
-    path('', registro, name='registro'),
-    path('', login, name='login'),
-    path('', hospedaje, name='hospedaje'),
-    path('', actividad, name='actividad'),
-    path('', gastronomia, name='gastronomia'),
-
+    path('registro/', registro, name='registro'),
+    path('login/', login, name='login'),
+    path('hospedaje/', hospedaje, name='hospedaje'),
+    path('actividad/', actividad, name='actividad'),
+    path('gastronomia/', gastronomia, name='gastronomia'),
 ]
